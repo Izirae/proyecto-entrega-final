3e071aa8457806b74d097c84f780621b1733a554
Izirae <lautaro.bazzola@gmail.com>
2022-06-01 02:26:15 -0300

    pilotos y equipos listo

---
+++ assets/js/equipos.js
@@ -0,0 +1,53 @@
+async function pilotosCard(){
+
+    await fetch('http://ergast.com/api/f1/current/constructors.json')
+    .then( (teams)=>{
+    return teams.json();
+
+    }).then((team)=>{
+
+    team.MRData.ConstructorTable.Constructors.forEach((data) => {
+
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const imgAuto = document.createElement("img");
+        const imgLogo = document.createElement("img");
+        const div = document.createElement("div");
+        const name = document.createElement("h3");
+        const divH = document.createElement("div");
+        const divImg = document.createElement("div");
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
+
+            path.setAttribute("d", "M 40 80 c 22 0 40 -22 40 -40 v 40 Z")
+            svg.setAttribute("class","arc")
+
+        li.appendChild(a);
+            a.className = "author-card";
+            a.target = "_blank"
+            a.href = data.url
+            a.appendChild(imgAuto);
+                imgAuto.src = "../img/teams/"+data.constructorId+".png";
+                imgAuto.className = "image";
+            a.appendChild(div);
+                div.className = "overlay";
+                div.appendChild(divH)
+                    divH.appendChild(svg)
+                        svg.appendChild(path)
+                    divH.className = "headerE";
+                    divH.appendChild(name);
+                        name.className = "nombreE"
+                        name.textContent = data.name
+                    divH.appendChild(divImg);
+                        divImg.className="headertext"
+                        divImg.appendChild(imgLogo)
+                            imgLogo.src = "../img/logos/"+data.constructorId+".png";
+                            imgLogo.className = "logo";
+    
+        card.appendChild(li)
+    })
+    })
+}
+
+pilotosCard()
+
---
+++ assets/js/pilotos.js
@@ -0,0 +1,62 @@
+async function pilotosCard(){
+
+    await fetch('http://ergast.com/api/f1/current/drivers.json')
+    .then( (drivers)=>{
+    return drivers.json();
+
+    }).then((driver)=>{
+
+    driver.MRData.DriverTable.Drivers.forEach((data) => {
+
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const imgPiloto = document.createElement("img");
+        const imgCasco = document.createElement("img");
+        const imgBandera = document.createElement("img");
+        const imgNumero = document.createElement("img");
+        const div = document.createElement("div");
+        const name = document.createElement("h3");
+        const divH = document.createElement("div");
+        const divHT = document.createElement("div");
+        const p = document.createElement("p");
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
+
+            path.setAttribute("d", "M 40 80 c 22 0 40 -22 40 -40 v 40 Z")
+            svg.setAttribute("class","arc")
+
+        li.appendChild(a);
+            a.className = "author-card";
+            a.href = data.url
+            a.appendChild(imgPiloto);
+                imgPiloto.src = "../img/drivers/"+data.driverId+".png";
+                imgPiloto.className = "image";
+            a.appendChild(div);
+                div.className = "overlay";
+                div.appendChild(divH)
+                    divH.appendChild(svg)
+                        svg.appendChild(path)
+                    divH.className = "header";
+                    divH.appendChild(imgCasco);
+                        imgCasco.src = "../img/helmets/"+data.driverId+".png";
+                        imgCasco.className = "helmet";
+                    divH.appendChild(divHT);
+                        divHT.className = "headertext";
+                        divHT.appendChild(name);
+                            name.className = "nombre"
+                            name.textContent = data.givenName + ' ' + data.familyName 
+                div.appendChild(p);
+                    p.appendChild(imgNumero);
+                        imgNumero.src = "../img/numbers/"+data.driverId+".png";
+                        imgNumero.className = "numb";
+                    p.appendChild(imgBandera);
+                        imgBandera.src = "../img/flags/"+data.nationality+".jpg";
+                        imgBandera.className = "flag";
+    
+        card.appendChild(li)
+    })
+    })
+}
+
+pilotosCard()
+
